5b998ebff2cccbe25724465dc158d8762e72ec4f
tendou-kuraga <agomez.software@gmail.com>
2020-02-12 18:11:34 -0500

    1 file modified

--- index.js
+++
@@ -1,7 +0,0 @@
-// Import stylesheets
-import './style.css';
-import 'https://d3-geomap.github.io/d3-geomap/d3-geomap.min.css';
-import 'https://unpkg.com/d3@5/dist/d3.min.js';
-import 'https://unpkg.com/topojson@3/dist/topojson.min.js';
-
-var map = d3.geomap().geofile('/d3-geomap/topojson/world/countries.json').draw(d3.select('#map'));
